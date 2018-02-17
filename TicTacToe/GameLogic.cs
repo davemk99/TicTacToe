74bfb1c10d53294f0e43138481a34dcee4e1c060
davem <davem@DESKTOP-D3836IO>
2018-02-17 23:54:03 +0400

    Add project files.

---
+++ TicTacToe/GameLogic.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    public enum GameState
+    {
+        FirstWin,
+        SecondWin,
+        Tie,
+        NotEnded,
+        NotValid
+    }
+
+    public enum Players
+    {
+        FirstPlayer,
+        SecondPlayer
+    }
+
+    public class GameLogic
+    {
+        public string PlayerSymbol { get; private set; }
+
+        private int[][] _gameMap;
+
+        public GameLogic()
+        {
+            PlayerSymbol = "X";
+            InitGameMap();
+        }
+
+        public Players GetPlayer()
+        {
+            if (PlayerSymbol == "X")
+            {
+                return Players.FirstPlayer;
+            }
+            else
+            {
+                return Players.SecondPlayer;
+            }
+        }
+
+        private void InitGameMap()
+        {
+            _gameMap = new int[3][];
+            for (int i = 0; i < 3; i++)
+            {
+                _gameMap[i] = new int[3];
+            }
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    _gameMap[i][j] = 0;
+                }
+            }
+        }
+
+        public GameState AddMove(int row, int column)
+        {
+            if (row >= 3 || row < 0)
+            {
+                throw new ArgumentException();
+            }
+            if (column >= 3 || column < 0)
+            {
+                throw new ArgumentException();
+            }
+
+            if (_gameMap[row][column] == 0)
+            {
+                if (PlayerSymbol == "X")
+                {
+                    _gameMap[row][column] = 1;
+                }
+                else
+                {
+                    _gameMap[row][column] = -1;
+                }
+            }
+            else
+            {
+                return GameState.NotValid;
+            }
+
+            return DoesGameEnd(_gameMap);
+        }
+
+        public void ChangeSymbol()
+        {
+            if (PlayerSymbol == "X")
+            {
+                PlayerSymbol = "O";
+            }
+            else
+            {
+                PlayerSymbol = "X";
+            }
+        }
+
+        private GameState WinWithRow(int[][] gameMap)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                var rowSum = gameMap[i].Sum();
+                var gameState = GameStateBySum(rowSum);
+                if (gameState != GameState.NotEnded)
+                {
+                    return gameState;
+                }
+            }
+            return GameState.NotEnded;
+        }
+
+        private GameState WinWithColumns(int[][] gameMap)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                var colSum = 0;
+                for (int j = 0; j < 3; j++)
+                {
+                    colSum += gameMap[j][i];
+                }
+
+                var gameState = GameStateBySum(colSum);
+                if (gameState != GameState.NotEnded)
+                {
+                    return gameState;
+                }
+            }
+            return GameState.NotEnded;
+        }
+
+        private GameState WinWithDiagonal(int[][] gameMap)
+        {
+            var diagonalSum = 0;
+            for (int i = 0; i < 3; i++)
+            {
+                diagonalSum += gameMap[i][i];
+            }
+            var gameState = GameStateBySum(diagonalSum);
+            if (gameState == GameState.NotEnded)
+            {
+                diagonalSum = 0;
+                for (int i = 0; i < 3; i++)
+                {
+                    diagonalSum += gameMap[i][2 - i];
+                }
+                return GameStateBySum(diagonalSum);
+            }
+            return gameState;
+        }
+
+        private GameState DoesGameEnd(int[][] gameMap)
+        {
+            var gameState = WinWithRow(gameMap);
+            if (gameState != GameState.NotEnded)
+            {
+                return gameState;
+            }
+            gameState = WinWithColumns(gameMap);
+            if (gameState != GameState.NotEnded)
+            {
+                return gameState;
+            }
+            gameState = WinWithDiagonal(gameMap);
+            if (gameState != GameState.NotEnded)
+            {
+                return gameState;
+            }
+            var zerosExist = gameMap.SelectMany(x => x).Any(x => x == 0);
+            if (zerosExist)
+            {
+                return GameState.NotEnded;
+            }
+            else
+            {
+                return GameState.Tie;
+            }
+        }
+
+        private GameState GameStateBySum(int sum)
+        {
+            if (sum == 3)
+            {
+                return GameState.FirstWin;
+            }
+            else if (sum == -3)
+            {
+                return GameState.SecondWin;
+            }
+            return GameState.NotEnded;
+        }
+    }
+}
---
+++ TicTacToe/MainWindow.xaml.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TicTacToe
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private GameLogic gameLogic;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            gameGrid.Loaded += GameGrid_Loaded;
+            gameLogic = new GameLogic();
+        }
+
+        private void GameGrid_Loaded(object sender, RoutedEventArgs e)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Button btn = new Button();
+                    btn.Name = string.Format("gameButton_{0}_{1}", i, j);
+                    Grid.SetRow(btn, i);
+                    Grid.SetColumn(btn, j);
+                    btn.FontSize = 20;
+                    btn.Click += Btn_Click;
+                    gameGrid.Children.Add(btn);
+                }
+            }
+        }
+
+        private void Btn_Click(object sender, RoutedEventArgs e)
+        {
+            var button = sender as Button;
+            var rowAndColumn = button.Name.Split('_').Skip(1).Take(2).Select(x => int.Parse(x)).ToArray();
+            var gameState = gameLogic.AddMove(rowAndColumn[0], rowAndColumn[1]);
+            ChangeFromGameState(gameState, button);
+        }
+
+        public void ChangeFromGameState(GameState gameState, Button button)
+        {
+            button.Foreground = GetForgroungByPlayer();
+            if (gameState == GameState.NotValid)
+            {
+                MessageBox.Show("Move is not valid");
+            }
+            else if (gameState == GameState.FirstWin)
+            {
+                button.Content = gameLogic.PlayerSymbol;
+                MessageBox.Show("First Player Wins");
+                InitFirstState();
+            }
+            else if (gameState == GameState.SecondWin)
+            {
+                button.Content = gameLogic.PlayerSymbol;
+                MessageBox.Show("Second Player Wins");
+                InitFirstState();
+            }
+            else if (gameState == GameState.Tie)
+            {
+                button.Content = gameLogic.PlayerSymbol;
+                MessageBox.Show("Tie");
+                InitFirstState();
+            }
+            else
+            {
+                button.Content = gameLogic.PlayerSymbol;
+                gameLogic.ChangeSymbol();
+            }
+        }
+
+        private Brush GetForgroungByPlayer()
+        {
+            var getPlayer = gameLogic.GetPlayer();
+            if (getPlayer == Players.FirstPlayer)
+            {
+                return new SolidColorBrush(Colors.Red);
+            }
+            else
+            {
+                return new SolidColorBrush(Colors.Blue);
+            }
+        }
+
+        private void InitFirstState()
+        {
+            for (int i = 0; i < gameGrid.Children.Count; i++)
+            {
+                var control = gameGrid.Children[i] as Button;
+                if (control != null && control.Name.Contains("gameButton"))
+                {
+                    control.Content = "";
+                }
+            }
+            gameLogic = new GameLogic();
+        }
+    }
+}
